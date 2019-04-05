c2a2b854fa9ce3a55552b2a7e2e55b40dd9bcf42
Zolboobayar Gantumur <zaccess33rus@gmail.com>
2019-04-05 18:13:41 +0800

    Add React app code

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+function Answer({ data: {answer, image} }) {
+  if (!answer || !image) return null;
+  return (
+    <div className="col">
+      <span>{answer}</span>
+      <img src={image} />
+    </div>
+  );
+}
 
 function App() {
-  return <input placeholder="Ask me something..." />;
+  const [answer, setAnswer] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const loadAnswer = () => {
+    fetch("https://yesno.wtf/api")
+      .then(res => res.json())
+      .then(res => {
+        setLoading(false);
+        setAnswer(res);
+      });
+  };
+
+  return (
+    <>
+      <input
+        disabled={loading}
+        placeholder="Ask me something..."
+        onKeyPress={event => {
+          if (event.key === "Enter") {
+            setAnswer(null);
+            loadAnswer();
+          }
+        }}
+      />
+      {answer && <Answer data={answer} />}
+    </>
+  );
 }
 
 export default App;
